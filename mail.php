e1b3f8973542b6a7c41599e165b355d1243d02d1
webster0 <37958472+webster0@users.noreply.github.com>
2018-04-15 12:07:06 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,12 +1,11 @@
 <?php
 
-$recepient = "agragregra@ya.ru";
+$recepient = "uahp@mail.ru";
 $sitename = "Название сайта";
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
-
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
